ddb501dd0ae0e5a3d32a25de7ed6056260edca5e
ArjunThakur7 <bannaarjun1234@gmail.com>
2025-05-10 20:31:06 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 #𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝙎𝙢𝙚𝙭𝙮 ᥫ᭡](https://t.me/SmexyOP)
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "25263708"
-api_hash = "9bde48b267ce65a576b478c0ff8c7bbb"
-bot_token = "7395475299:AAF4UK-zkoanFNX2s0p4UdsGXIQF5POr8gU"
+api_id = "24853719"
+api_hash = "d88df23f822a7e3bf60157a0f3a8a11d"
+bot_token = "7832011152:AAEae4rRmHGCKlyoiZh-E6gSi2IOrlYNEaM"
 # Bot Username = "https://t.me/SmexyTxtLeechingBot"
